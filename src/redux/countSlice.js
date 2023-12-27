cf50919802a59a2404f0de2e98f38337cb2f2a78
Liya Lenin <liyaleninliya@gmail.com>
2023-12-27 12:38:54 +0530

    updated

--- src/redux/countSlice.js
+++ src/redux/countSlice.js
@@ -1,21 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const countSlice=createSlice({
-    name:'counter',
-    initialState:{
-        count:0
+const countSlice = createSlice({
+    name: 'counter',
+    initialState: {
+        count: 0
     },
-    reducers:{
-        increment:(state)=>{
-        state.count+=1
+    reducers: {
+        increment: (state) => {
+            state.count += 1
         },
-        decrement:(state)=>{
-            state.count-=1
-            },
-            reset:(state)=>{
-                state.count=0
-            }
+        decrement: (state) => {
+            state.count -= 1
+        },
+        reset: (state) => {
+            state.count = 0
+        },
+        incrementByAmount: (state, action) => {
+            state.count +=action.payload
+        }
     }
 })
-export const {increment,decrement,reset}=countSlice.actions
+export const { increment, decrement, reset,incrementByAmount } = countSlice.actions
 export default countSlice.reducer
